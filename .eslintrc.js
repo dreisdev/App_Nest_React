330870f8e3eedde5b329aa2458e761fa4f3e8ef8
dreisdev <dreis.dev@outlook.com>
2024-03-12 14:43:30 -0300

    add prisma orm on project and database configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,19 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        singleQuote: true,
+        arrowParens: 'avoid',
+        useTabes: true,
+        trailingComma: true,
+      },
+      {
+        usePrettierrc: false,
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
