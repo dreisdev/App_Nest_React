412c6287c4ac9a26574b9b9e985c112c2d1f4c44
dreisdev <dreis.dev@outlook.com>
2024-03-13 14:09:44 -0300

    structure project

--- prisma/migrations/20240312181714_task/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-
-    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
-);
--- src/todolist/todolist.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TodolistService } from './todolist.service';
-import { TodolistController } from './todolist.controller';
-
-@Module({
-  controllers: [TodolistController],
-  providers: [TodolistService],
-})
-export class TodolistModule {}
