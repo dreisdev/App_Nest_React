28a2839520150fd8fffdd7defe51c876f43ca54c
dreisdev <dreis.dev@outlook.com>
2024-03-12 23:37:57 -0300

    cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors({
+    origin: '*',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  }));
+
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
